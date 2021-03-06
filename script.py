0c61c49135dcddf2f6d4133306df13acd0e32315
RaymondKerley <raymondkerley@outlook.com>
2021-03-06 18:24:26 +0000

    Create list of dictionaries for folders in repository

--- script.py
+++ script.py
@@ -77,19 +77,19 @@ class LocalUpdateSections(object):
 ################################################
 
 
-courseid = "14"  # Exchange with valid id.
+courseid = "2"  # Exchange with valid id.
 # Get all sections of the course.
 sec = LocalGetSections(courseid)
 
 # Output readable JSON, but print only summary
-print(json.dumps(sec.getsections[1]['summary'], indent=4, sort_keys=True))
+#rkprint(json.dumps(sec.getsections[1]['summary'], indent=4, sort_keys=True))
 
 # Split the section name by dash and convert the date into the timestamp, it takes the current year, so think of a way for making sure it has the correct year!
 month = parser.parse(list(sec.getsections)[1]['name'].split('-')[0])
 # Show the resulting timestamp
-print(month)
+#rkprint(month)
 # Extract the week number from the start of the calendar year
-print(month.strftime("%V"))
+#rkprint(month.strftime("%V"))
 
 #  Assemble the payload
 data = [{'type': 'num', 'section': 0, 'summary': '', 'summaryformat': 1, 'visible': 1 , 'highlight': 0, 'sectionformatoptions': [{'name': 'level', 'value': '1'}]}]
@@ -107,4 +107,53 @@ data[0]['section'] = 1
 sec_write = LocalUpdateSections(courseid, data)
 
 sec = LocalGetSections(courseid)
-print(json.dumps(sec.getsections[1]['summary'], indent=4, sort_keys=True))
+#rkprint(json.dumps(sec.getsections[1]['summary'], indent=4, sort_keys=True))
+
+list_of_dicts = []
+dictionary = {}
+count = 0
+
+import os
+import re
+
+for folder , sub_folders , files in os.walk("/workspace/MoodleAutomation"):
+    if "wk" in folder:
+        info = re.search(r'\d+\w*', folder)
+        list_of_dicts.append(dictionary.copy())
+        list_of_dicts[count]["week number"] = info.group()
+        for sub_fold in sub_folders:
+            break
+        for f in files:
+            if f.endswith(".html"):
+                list_of_dicts[count]["index"] = f
+            if f.endswith(".md"):
+                list_of_dicts[count]["slides"] = f
+            if f.endswith(".pdf"):
+                list_of_dicts[count]["pdf"] = f
+        count += 1
+    
+    
+
+    
+    #if "wk" in folder:
+     #   week_data_dictionary["{folder}"] = "wk{folder}"
+
+
+    #rkprint("Currently looking at folder: "+ folder)
+    #rkprint('\n')
+    #rkprint("THE SUBFOLDERS ARE: ")
+    #rkfor sub_fold in sub_folders:
+        #rkprint("\t Subfolder: "+sub_fold )
+
+    #rkprint('\n')
+
+    #rkprint("THE FILES ARE: ")
+    #rkfor f in files:
+        #rkprint("\t File: "+f)
+    #rkprint('\n')
+
+    # Now look at subfolders
+
+
+    #print(week_data_dictionary)
+print(list_of_dicts)
