7c39adea2db57d811b56c1d4c36cf25d3de68d2e
RaymondKerley <raymondkerley@outlook.com>
2021-03-14 23:22:47 +0000

    Final Code

--- script.py
+++ script.py
@@ -10,17 +10,11 @@ from dateutil.relativedelta import relativedelta
 
 
 
-
-
-
-
-
-
 # Module variables to connect to moodle api:
 # Insert token and URL for your site here.
 # Mind that the endpoint can start with "/moodle" depending on your installation.
-KEY = "8cc87cf406775101c2df87b07b3a170d"
-URL = "https://034f8a1dcb5c.eu.ngrok.io"
+KEY = "bc7ad59923ad95f17dd955868790ccb5"
+URL = "http://f0ae7213ef73.eu.ngrok.io/"
 ENDPOINT = "/webservice/rest/server.php"
 
 
@@ -67,9 +61,11 @@ def call(fname, **kwargs):
     return response
 
 
+
 ################################################
 # Rest-Api classes
 ################################################
+
 
 
 class LocalGetSections(object):
@@ -91,23 +87,22 @@ class LocalUpdateSections(object):
 
 
 ############################################################
+### Setup
+############################################################
 
-courseid = "2"  # Set our Course ID
+
+
+courseid = "7"  # Set our Course ID
 sec = LocalGetSections(courseid) # Get all sections of the course.
 data = [{'type': 'num', 'section': 0, 'summary': '', 'summaryformat': 1, 'visible': 1 , 'highlight': 0, 'sectionformatoptions': [{'name': 'level', 'value': '1'}]}] #  Assemble the payload
-
-
-##################################################
-
-
-
-
 
 
 
 #############################################################
 ### Create a list of dictionaries for folders in repository
 #############################################################
+
+
 
 list_of_folder_dictionaries = [] #Create an empty list to add our folder dictionaries to
 counter_1 = 0 #Create counter for the indices of our list
@@ -128,13 +123,13 @@ for folder, sub_folders, files in os.walk(os.getcwd()):   #Walk through folders 
                 list_of_folder_dictionaries[counter_1]["pdf"] = f #If the file is a .pdf, assign that value to the "pdf" key in the dictionary
         counter_1 += 1 #Increase the counter by 1 to track the indices of our list
 
-#print(list_of_folder_dictionaries)
-
 
 
 #############################################################
 ### Create list of dictionaries with hash and date of videos
 #############################################################
+
+
 
 list_of_video_dictionaries = [] #Create an empty list to add our video dictionaries to
 counter_2 = 0 #Create counter for the indices of our list
@@ -154,8 +149,6 @@ for video in videos: #iterate through data from each video found in the class = 
     list_of_video_dictionaries[counter_2]["date"] = video_date #Assign the date to the "date" key in the dictionary
     counter_2 += 1 #Increase the counter by 1 to track the indices of our list
 
-#print(list_of_video_dictionaries)
-
 
 
 #############################################################################
@@ -163,78 +156,87 @@ for video in videos: #iterate through data from each video found in the class = 
 #############################################################################
 
 
+
 list_of_datetime_dictionaries = [{}] #Create a list with one empty dictionary in it. Used to add our datetime dictionaries
 counter_3 = 0 #Create counter for the indices of our list
 
-
 for j in range(1,26): 
     list_of_datetime_dictionaries.append({}) #Add a new dictionary to the list each time we find a new video
-    week_start = parser.parse(list(sec.getsections)[j]['name'].split('-')[0]) # Split the section name by dash and convert the date into the timestamp
-    week_end = parser.parse(list(sec.getsections)[j]['name'].split('-')[1]) # Split the section name by dash and convert the date into the timestamp
-    if week_start > datetime.datetime(2021,8,1):
-        week_start -= relativedelta(years=1)
-    if week_end > datetime.datetime(2021,8,1):
-        week_end -= relativedelta(years=1)
-    list_of_datetime_dictionaries[counter_3]["start_date"] = week_start
-    list_of_datetime_dictionaries[counter_3]["end_date"] = week_end
-    counter_3 += 1
-print(list_of_datetime_dictionaries)
+    week_start = parser.parse(list(sec.getsections)[j]['name'].split('-')[0]) # Split the section name by dash and convert the date into the timestamp, and set as start date
+    week_end = parser.parse(list(sec.getsections)[j]['name'].split('-')[1]) # Split the section name by dash and convert the date into the timestamp, and set as end date
+    if week_start > datetime.datetime(2021,8,1): #Check to see if start date is after August 1st
+        week_start -= relativedelta(years=1) #If it is, deduct a year from the start date to ensure we are dealing with the correct year
+    if week_end > datetime.datetime(2021,8,1): #Check to see if end date is after August 1st
+        week_end -= relativedelta(years=1) #If it is, deduct a year from the end date to ensure we are dealing with the correct year
+    list_of_datetime_dictionaries[counter_3]["start_date"] = week_start #Add the start date to the list of dictionaries
+    list_of_datetime_dictionaries[counter_3]["end_date"] = week_end #Add the end date to the list of dictionaries
+    counter_3 += 1 #Increase the counter by 1 to track the indices of our list
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-################################
+############################################
 ### Update html links on Moodle
-################################
-
-
-# for d in list_of_folder_dictionaries: 
-#     summary = '<a href="https://mikhail-cct.github.io/ca3-test/wk{}/">Week{} Slides</a><br><a href="https://mikhail-cct.github.io/ca3-test/wk{}.pdf/">Week{} PDF</a><br>'.format(d["week_number"], d["week_number"], d["week_number"], d["week_number"])
-#     data[0]['summary'] = summary
-#     data[0]['section'] = d["week_number"]
-#     sec_write = LocalUpdateSections(courseid, data)
-
-#print(summary)
+############################################
 
 
 
-# week_counter = 1
-# video_counter = 1
-
-# for video in videos:
-#     while True:
-#         if list_of_video_dictionaries[week_counter]["date"] >= list_of_datetime_dictionaries[video_counter]["start_date"] and list_of_video_dictionaries[week_counter]["date"] <= list_of_datetime_dictionaries[video_counter]["end_date"]:
-#             summary = sec.getsections[week_counter]["summary"]
-#             summary +='<a href="https://drive.google.com/file/d/{}/">Week{} Video</a>'.format(list_of_video_dictionaries[week_counter]["hash"], week_counter)
-#             data[0]['summary'] = ''
-#             data[0]['section'] = week_counter
-#             sec_write = LocalUpdateSections(courseid, data)
-#             data[0]['summary'] = summary
-#             sec_write = LocalUpdateSections(courseid, data)
-#             break
-#         elif video_counter == len(videos):
-#             break
-#         else:
-#             video_counter += 1
-#     if week_counter < 15: #len(list_of_video_dictionaries):
-#         week_counter +=1
-
-# print(summary)
+for d in list_of_folder_dictionaries: 
+    summary = '<a href="https://mikhail-cct.github.io/ca3-test/wk{wk}/">Week {wk} Slides</a><br><a href="https://mikhail-cct.github.io/ca3-test/wk{wk}.pdf/">Week {wk} PDF</a><br>'.format(wk =d["week_number"])
+    data[0]['summary'] = summary #set the summary variable as the html text we want to write to our Moodle page 
+    data[0]['section'] = d["week_number"] #Set the week number, to update the correct week
+    sec_write = LocalUpdateSections(courseid, data) #Write the new information to our Moodle page
 
 
-#for i in range(1,13):
-   #print(json.dumps(sec.getsections[i], indent=4, sort_keys=True))
-    #print(json.dumps(sec.getsections[i]["sectionnum"], indent=4, sort_keys=True))
-    #print(json.dumps(sec.getsections[i]["summary"], indent=4, sort_keys=True))
+
+############################################
+### Update videos on Moodle
+############################################
 
 
+week_counter = 1 #Create a counter to track the week number we are looking at
+video_counter = 1 #Create a counter to track the video we are looking at
+
+'''
+for video in videos: #Loop through each of the videos on the Google drive
+    while True: 
+        #Check if the date of the video falls between the start date and end date of a Week number on Moodle
+        if list_of_video_dictionaries[week_counter]["date"] >= list_of_datetime_dictionaries[video_counter]["start_date"] and list_of_video_dictionaries[week_counter]["date"] <= list_of_datetime_dictionaries[video_counter]["end_date"]:
+            # summary = sec.getsections[week_counter]["summary"] # Temporarily hold the current Moodle summary in this variable
+            # if list_of_video_dictionaries[week_counter]["hash"] not in summary: #Check if the video has already been added to Moodle
+            # #If not, update the summary variable to include the video link
+            #     summary +='<a href="https://drive.google.com/file/d/{hash}/">Week {wk} Video</a><br>'.format(hash = list_of_video_dictionaries[week_counter]["hash"], wk = week_counter)
+            data[0]['summary'] = '' #Set the section summary to blank so we can clear the summary details on Moodle
+            data[0]['section'] = week_counter #Set the week number
+            sec_write = LocalUpdateSections(courseid, data) #Write to Moodle to clear each of the section summaries
+            #data[0]['summary'] = summary #Set the section summary the new summary so we can update the summary details on Moodle
+            #sec_write = LocalUpdateSections(courseid, data) #Write to Moodle to update each of the section summaries
+            break #Break from the While loop
+        elif video_counter == len(videos): #Break from the While loop if we have visited each of the videos
+            break #Break from the While loop
+        else:
+            video_counter += 1 #Increase the video counter so we can check each video in each week.
+    if week_counter < 17: #Exit the loop if we have reached the last week of the semester.
+        week_counter +=1 #Increase the week counter so we can check each video in each week.
+'''
+
+
+for video in videos: #Loop through each of the videos on the Google drive
+    while True: 
+        #Check if the date of the video falls between the start date and end date of a Week number on Moodle
+        if list_of_video_dictionaries[week_counter]["date"] >= list_of_datetime_dictionaries[video_counter]["start_date"] and list_of_video_dictionaries[week_counter]["date"] <= list_of_datetime_dictionaries[video_counter]["end_date"]:
+            summary = sec.getsections[week_counter]["summary"] # Temporarily hold the current Moodle summary in this variable
+            if list_of_video_dictionaries[week_counter]["hash"] not in summary: #Check if the video has already been added to Moodle
+            #If not, update the summary variable to include the video link
+                summary +='<a href="https://drive.google.com/file/d/{hash}/">Week {wk} Video</a><br>'.format(hash = list_of_video_dictionaries[week_counter]["hash"], wk = week_counter)
+            data[0]['summary'] = '' #Set the section summary to blank so we can clear the summary details on Moodle
+            data[0]['section'] = week_counter #Set the week number
+            sec_write = LocalUpdateSections(courseid, data) #Write to Moodle to clear each of the section summaries
+            data[0]['summary'] = summary #Set the section summary the new summary so we can update the summary details on Moodle
+            sec_write = LocalUpdateSections(courseid, data) #Write to Moodle to update each of the section summaries
+            break #Break from the While loop
+        elif video_counter == len(videos): #Break from the While loop if we have visited each of the videos
+            break #Break from the While loop
+        else:
+            video_counter += 1 #Increase the video counter so we can check each video in each week.
+    if week_counter < 12: #Exit the loop if we have reached the last week of the semester.
+        week_counter +=1 #Increase the week counter so we can check each video in each week.
